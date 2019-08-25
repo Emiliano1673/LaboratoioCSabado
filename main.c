479b8a5bb3999ca6059740277f2d1ba336db1cb8
Emiliano Sanchez <foreveremiliano9@gmail.com>
2019-08-25 14:07:48 -0500

    Tercer commit proyecto base

--- main.c
+++ main.c
@@ -4,13 +4,16 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	float x,r1;		//la variable x es la variable que se usara para las operaciones, r1 es la variable en la que se almacenara la respuesta 1
+	float x,r1,r2;		//la variable x es la variable que se usara para las operaciones, r1 es la variable en la que se almacenara la respuesta 1
 	
 	printf("Introduce el numero: \n");
 	scanf("%f",&x);						//Se pide el numero x con el que se efectuaran las operaciones
 	r1=((x-1)/4)-((x-5)/36);			//Se efectuan las operaciones
+	r2=(6*(((x+1)/8)-(((2*x)-3)/16)));
 	
 	printf("\nResultado 1: %.2f",r1);	//Se imprime el resultado 
+	printf("\nResultado 2: %.2f",r2);	
+	
 	//Comentario para identificar branch Practica 3
 	return 0;
 }
