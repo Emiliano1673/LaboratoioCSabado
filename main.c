e6267c666aa54c33a557224c1a925ae0d1f0316b
Emiliano Sanchez <foreveremiliano9@gmail.com>
2019-11-16 10:57:47 -0600

    Primer commit Practica 15

--- main.c
+++ main.c
@@ -4,5 +4,43 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+
+	char s[20];
+	char t[20];
+	scanf("%[^\n]%*c", s);	
+	int i=0;
+	for (i=0;i<20;i++){
+		t[i]=s[i];
+	}
+	
+	printf("%s",t);
+	
+	FILE *caso;
+	int a,b;
+		
+	scanf("%d%d", &a, &b);	
+	caso=fopen("archivo.txt", "a+");
+	fprintf(caso,"%d\n",a);
+	fprintf(caso,"%d\n",b);
+	fclose(caso);
+	
+	/*caso=fopen("archivo.txt", "r");
+	while(1){
+		c = fgetc(caso);
+      if( feof(caso) ) { 
+         break ;
+      }
+      printf("%c", c);
+   }
+   fclose(caso);
+	*/
+	
+	//scanf("%[^\n]%*c", s);
+	
+	FILE *num;
+	num=fopen("archivo2.txt", "a+");
+	fprintf(num,"La suma de %d + %d es: %d\n",a,b,a+b);
+	fclose(num);
+	
 	return 0;
 }
